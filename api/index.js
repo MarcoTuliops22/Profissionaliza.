b3cd0df6df58393c3a9fb896d47f7e1eb916349d
Marco Túlio <marcotuliops22@gmail.com>
2026-07-30 03:39:03 -0300

    chore: remove modulo guardian e scripts de log instaveis

--- api/index.js
+++ api/index.js
@@ -1,12 +1,7 @@
 const visitors = require('./visitors');
-const guardian = require('./guardian');
 
 module.exports = async function handler(req, res) {
   const pathname = new URL(req.url || '/', 'http://localhost').pathname;
-
-  if (pathname === '/api/guardian' || pathname === '/guardian') {
-    return guardian(req, res);
-  }
 
   if (pathname === '/api/visitors' || pathname === '/visitors') {
     return visitors(req, res);
@@ -15,6 +10,6 @@ module.exports = async function handler(req, res) {
   res.status(200).json({
     success: true,
     message: 'API Profissionaliza disponível.',
-    endpoints: ['/api/visitors', '/api/guardian']
+    endpoints: ['/api/visitors']
   });
 };
